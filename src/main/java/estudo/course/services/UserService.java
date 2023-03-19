c7f8d088fb365e5ffef295da61d135e930a337bc
saagas-code <saagas.code@gmail.com>
2023-03-19 04:27:43 -0300

    feat: exception handling

--- src/main/java/estudo/course/services/UserService.java
+++ src/main/java/estudo/course/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import estudo.course.entities.User;
 import estudo.course.repositories.UserRepository;
+import estudo.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +23,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
